ba40e2b5f371b97de9e9de449febdd6022388898
Timurs Cirganovics <you@example.com>
2023-12-09 22:28:05 +0200

    SplashScreen, Intro, SignUp & SignIn activities

---
+++ app/src/main/java/com/example/colaboraboard/SignUpActivity.kt
@@ -0,0 +1,40 @@
+package com.example.colaboraboard
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toolbar
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        //Hides the status bar on different android versions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        setupActionBar()
+    }
+
+    //Set the button to go bac to the Intro screen
+    private fun setupActionBar(){
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_sign_up_activity)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+    }
+}
---
+++ app/src/main/java/com/example/colaboraboard/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.example.colaboraboard
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.TextView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //Hides the status bar on different android versions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        //Applying custom font to title on the splashscreen
+        val typeface: Typeface = Typeface.createFromAsset(assets, "carbon_bl.otf")
+        val title = findViewById<TextView>(R.id.tvAppName)
+        title.typeface = typeface
+        //Move from SplashScreen to Intro screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2500)
+    }
+}
