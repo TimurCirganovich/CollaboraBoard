3dc6c5fb72d63ed090830fa7bd7a01ef7681cf5e
Timurs Cirganovics <you@example.com>
2024-01-04 20:30:42 +0200

    Edit project name to "CollaboraBoard" (as it should be)

---
+++ app/src/main/java/com/example/collaboraboard/activities/SignUpActivity.kt
@@ -0,0 +1,145 @@
+package com.example.collaboraboard.activities
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import com.example.collaboraboard.R
+import com.example.collaboraboard.databinding.ActivitySignUpBinding
+import com.example.collaboraboard.firebase.FirestoreClass
+import com.example.collaboraboard.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class SignUpActivity : BaseActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Initialize the Firebase variable
+        auth = FirebaseAuth.getInstance()
+
+        //Hides the status bar on different android versions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        setupActionBar()
+    }
+
+    //Set the button to go bac to the Intro screen
+    private fun setupActionBar(){
+        setSupportActionBar(binding.toolbarSignUpActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        binding.toolbarSignUpActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        binding.btnSignUp.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser(){
+        val name: String = binding.etName.text.toString().trim { it <= ' ' }
+        val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+        val password: String = binding.etPassword.text.toString().trim { it <= ' ' }
+
+        if(validateForm(name, email, password)){
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registeredEmail = firebaseUser.email!!
+                        val user = User(firebaseUser.uid, name, registeredEmail)
+                        FirestoreClass().registerUser(this, user)
+                    } else {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String) : Boolean{
+        return when{
+            TextUtils.isEmpty(name)->{
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email)->{
+                showErrorSnackBar("Please enter an email address")
+                false
+            }
+            TextUtils.isEmpty(password)->{
+                showErrorSnackBar("Please enter a password")
+                false
+            } else ->{
+                true
+            }
+        }
+    }
+
+    fun userRegisteredSuccess(){
+        Toast.makeText(
+            this,
+            "You have successfully registered!",
+            Toast.LENGTH_LONG
+        ).show()
+        hideProgressDialog()
+        auth.signOut()
+        autoSignIn()
+    }
+
+    private fun autoSignIn(){
+        val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+        val password: String = binding.etPassword.text.toString().trim { it <= ' ' }
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                hideProgressDialog()
+                if (task.isSuccessful) {
+                    // Sign in success
+                    Log.d("SignUp", "signInWithEmail:success")
+                    FirestoreClass().loadUserData(this)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("SignUp", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this@SignUpActivity,
+                        task.exception!!.message,
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    hideProgressDialog()
+                }
+            }
+    }
+
+    fun autoSignInSuccess(){
+        hideProgressDialog()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ColaboraBoard"
+rootProject.name = "CollaboraBoard"
 include(":app")
